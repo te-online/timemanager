7a209afffcb4c1114b181be8bfa655a16662e391
Thomas Ebert <thomas.ebert@te-online.net>
2016-12-11 21:44:50 +0100

    Translate first methods for api endpoint.

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -5,5 +5,6 @@ return [
 		['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
 		['name' => 'api#get', 'url' => '/api/items', 'verb' => 'GET'],
 		['name' => 'api#post', 'url' => '/api/items', 'verb' => 'POST'],
+		['name' => 'api#updateObjects', 'url' => '/api/updateObjects', 'verb' => 'POST']
 	]
 ];
--- controller/apicontroller.php
+++ controller/apicontroller.php
@@ -71,4 +71,107 @@ class ApiController extends Controller {
 		$client = $this->clientMapper->insert($client);
 		return new DataResponse($client->toArray());
 	}
+
+	/**
+	 * @NoAdminRequired
+	 *
+	 * @param json $data
+	 * @return DataResponse
+	 */
+	function updateObjects($postdata) {
+		$postdata = json_encode($postdata);
+		$entities = ["clients", "projects", "tasks", "times"];
+
+		if(!$postdata) {
+			return new DataResponse('404');
+		}
+		if(empty($postdata->lastCommit)) {
+			return new DataResponse('500; ' . json_encode(["error" => "Commit is mandatory."]));
+		}
+		if(empty($postdata->data)) {
+			return new DataResponse('500; ' . json_encode(["error" => "Data is mandatory."]));
+		}
+
+		$noData = true;
+
+		foreach($entities as $entity) {
+			if(empty($postdata->data[$entity]) || empty($postdata->data[$entity]->created) || empty($postdata->data[$entity]->updated) || empty($postdata->data[$entity]->deleted)) {
+				return new DataResponse('500; ' . json_parse(["error" => $entity . " is mandatory."]));
+			}
+			if(count($postdata->data[$entity]->created) > 0 || count($postdata->data[$entity]->updated) > 0 || count($postdata->data[$entity]->deleted) > 0) {
+				$noData = false;
+			}
+		}
+
+		$clientCommit = $postdata->lastCommit;
+		$missions = array();
+
+		if(!$noData) {
+
+			$commit = UUID.v4(); // TODO
+
+			foreach($entities as $entity) {
+				// For all entities take the created objects
+				$created = $postdata->data[$entity]->created;
+				// try to create object, if object already present -> move it to the changed array
+				foreach($created as $object) {
+					// mark with current commit
+					$object['commit'] = $commit;
+					// Add or update object here.
+					$storageHelper->addOrUpdateObject($object, $entity));
+				}
+				// For all entities take the changed objects
+				$updated = $postdata->data[$entity]->updated;
+				// if current object commit <= last commit delivered by client
+				foreach($updated as $object) {
+					// mark with current commit
+					$object['commit'] = $clientCommit;
+					$object['desiredCommit'] = $commit;
+					// Add or update object here.
+					$storageHelper->addOrUpdateObject($object, $entity));
+				}
+				// For all entities take the deleted objects
+				$deleted = $postdata->data[$entity]->deleted;
+				// if current object commit <= last commit delivered by client
+				foreach($deleted as $object) {
+					// mark with current commit
+					$object['commit'] = $clientCommit;
+					$object['desiredCommit'] = $commit;
+					// Add or update object here.
+					$storageHelper->maybeDeleteObject($object, $entity));
+				}
+			}
+
+		}
+
+		$results = array();
+
+		foreach($entities as $entity) {
+			$results[] = $cleaner->clean($storageHelper.getObjectsAfterCommit($entity, $clientCommit));
+		}
+
+		$lastCommit = $storageHelper->getLatestCommit();
+		$response = array( "data" => [] );
+
+		$index = 0;
+		foreach($entities as $entity) {
+			$response->data[$entity] = $results[index];
+			$index++;
+		}
+
+		if(!n$oData) {
+			$response['commit'] = $commit;
+			$storageHelper->insertCommit($commit);
+		} else {
+			$response['commit'] = $lastCommit;
+		}
+
+		return new DataResponse(json_encode($response));
+
+
+		// .catch(function(err) {
+		// 	console.error(err.stack);
+		// 	res.status(500).send(JSON.stringify([{ "error": err.error }], null, 2));
+		// });
+	}
 }
