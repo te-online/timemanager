9bf630c070b1e8b8981cafea9883b1abadac9d0e
Thomas Ebert <thomas.ebert@te-online.net>
2018-01-21 11:39:10 +0100

    Disable old demo stuff.

--- appinfo/register_command.php
+++ appinfo/register_command.php
@@ -1,4 +1,4 @@
 <?php
 $app = new \OCP\AppFramework\App('timemanager');
 /** @var Symfony\Component\Console\Application $application */
-$application->add(new OCA\TimeManager\Command\ListClients($app->getContainer()->query('OCA\TimeManager\Db\ClientMapper')));
+// $application->add(new OCA\TimeManager\Command\ListClients($app->getContainer()->query('OCA\TimeManager\Db\ClientMapper')));
--- command/listclients.php
+++ command/listclients.php
@@ -1,47 +1,47 @@
 <?php
 
-namespace OCA\TimeManager\Command;
+// namespace OCA\TimeManager\Command;
 
-use OCA\TimeManager\Db\Client;
-use OCA\TimeManager\Db\ClientMapper;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+// use OCA\TimeManager\Db\Client;
+// use OCA\TimeManager\Db\ClientMapper;
+// use Symfony\Component\Console\Command\Command;
+// use Symfony\Component\Console\Helper\Table;
+// use Symfony\Component\Console\Input\InputArgument;
+// use Symfony\Component\Console\Input\InputInterface;
+// use Symfony\Component\Console\Output\OutputInterface;
 
-class ListClients extends Command {
-	/** @var ClientMapper */
-	protected $clientMapper;
+// class ListClients extends Command {
+// 	/** @var ClientMapper */
+// 	protected $clientMapper;
 
-	function __construct(ItemMapper $clientMapper) {
-		$this->clientMapper = $clientMapper;
-		parent::__construct();
-	}
+// 	function __construct(ItemMapper $clientMapper) {
+// 		$this->clientMapper = $clientMapper;
+// 		parent::__construct();
+// 	}
 
-	protected function configure() {
-		$this
-			->setName('timemanager:listclients')
-			->setDescription('List timemanager clients by user')
-			->addArgument(
-				'user',
-				InputArgument::REQUIRED,
-				'owner of timemanager clients'
-			);
-	}
+// 	protected function configure() {
+// 		$this
+// 			->setName('timemanager:listclients')
+// 			->setDescription('List timemanager clients by user')
+// 			->addArgument(
+// 				'user',
+// 				InputArgument::REQUIRED,
+// 				'owner of timemanager clients'
+// 			);
+// 	}
 
-	protected function execute(InputInterface $input, OutputInterface $output) {
-		$userId = $input->getArgument('user');
+// 	protected function execute(InputInterface $input, OutputInterface $output) {
+// 		$userId = $input->getArgument('user');
 
-		$clients = $this->clientMapper->findByUser($userId);
-		$clients = array_map(function(Client $client) {
-			return $client->toArray();
-		}, $clients);
+// 		$clients = $this->clientMapper->findByUser($userId);
+// 		$clients = array_map(function(Client $client) {
+// 			return $client->toArray();
+// 		}, $clients);
 
-		$table = new Table($output);
-		$table
-			->setHeaders(['Uuid', 'Name', 'Note'])
-			->setRows($clients);
-		$table->render();
-	}
-}
+// 		$table = new Table($output);
+// 		$table
+// 			->setHeaders(['Uuid', 'Name', 'Note'])
+// 			->setRows($clients);
+// 		$table->render();
+// 	}
+// }
