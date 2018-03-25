8610578a2c30953e1959ca8890aa5aab2d1d01f9
Thomas Ebert <thomas.ebert@te-online.net>
2018-03-25 01:46:14 +0100

    Fix empty or unknown commit would result in no created items returned by API.

--- db/commitmapper.php
+++ db/commitmapper.php
@@ -35,17 +35,38 @@ class CommitMapper extends Mapper {
 	}
 
 	function getCommitsAfter($commit) {
-		$sql = 'SELECT * ' .
+		// Find the given commit first, see if we have it.
+		$sql = 'SELECT `commit` ' .
 				'FROM `' . $this->tableName . '` ' .
-				'WHERE `user_id` = ? AND `created` >'.
-				' (SELECT `created` FROM `' . $this->tableName . '` WHERE `commit` = ? LIMIT 1) '.
-				'ORDER BY `created`;';
-		$commits = array_map(
-			function($commit) {
-				return $commit->toString();
-			},
-			$this->findEntities($sql, [$this->userId, $commit])
-		);
+				'WHERE `user_id` = ? AND `commit` = ? LIMIT 1;';
+		$givenCommit = $this->findEntities($sql, [$this->userId, $commit]);
+
+		// The given commit is found, all fine.
+		if(count($givenCommit) > 0) {
+			$sql = 'SELECT * ' .
+					'FROM `' . $this->tableName . '` ' .
+					'WHERE `user_id` = ? AND `created` >'.
+					' (SELECT `created` FROM `' . $this->tableName . '` WHERE `commit` = ? LIMIT 1) '.
+					'ORDER BY `created`;';
+			$commits = array_map(
+				function($commit) {
+					return $commit->toString();
+				},
+				$this->findEntities($sql, [$this->userId, $commit])
+			);
+		} else {
+			// The given commit is unknown. All commits are applicable.
+			$sql = 'SELECT * ' .
+					'FROM `' . $this->tableName . '` ' .
+					'WHERE `user_id` = ? '.
+					'ORDER BY `created`;';
+			$commits = array_map(
+				function($commit) {
+					return $commit->toString();
+				},
+				$this->findEntities($sql, [$this->userId])
+			);
+		}
 		return $commits;
 	}
 }
