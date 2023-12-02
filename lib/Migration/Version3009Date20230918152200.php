91258996aae889a7bfdfcaba279f795f86068a76
Thomas Ebert <thomas.ebert@te-online.net>
2023-12-02 22:04:17 +0100

    Fix up migrations

--- lib/Migration/Version3009Date20230918152200.php
+++ lib/Migration/Version3009Date20230918152200.php
@@ -14,13 +14,11 @@ use OCP\Migration\SimpleMigrationStep;
 /**
  * Auto-generated migration step: Please modify to your needs!
  */
-class Version3009Date20231202210000 extends SimpleMigrationStep
-{
+class Version3009Date20230918152200 extends SimpleMigrationStep {
 	/** @var IDBConnection */
 	private $db;
 
-	public function __construct(IDBConnection $db)
-	{
+	public function __construct(IDBConnection $db) {
 		$this->db = $db;
 	}
 
@@ -29,8 +27,7 @@ class Version3009Date20231202210000 extends SimpleMigrationStep
 	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
 	 * @param array   $options
 	 */
-	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void
-	{
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
 	}
 
 	/**
@@ -39,29 +36,20 @@ class Version3009Date20231202210000 extends SimpleMigrationStep
 	 * @param array   $options
 	 * @return null|ISchemaWrapper
 	 */
-	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper
-	{
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
 		/** @var ISchemaWrapper $schema */
 		$schema = $schemaClosure();
 
 		$table = $schema->getTable("timemanager_share");
 
 		$table->addColumn("recipient_id", "string", [
-			"notnull" => true,
+			"notnull" => false,
 			"length"  => 64,
 		]);
 		$table->addColumn("recipient_type", "string", [
-			"notnull" => true,
+			"notnull" => false,
 			"length"  => 64,
 		]);
-
-		$table->dropIndex("timemanager_share_with_index");
-		$table->addIndex(["recipient_id", "recipient_type"], "timemanager_share_with_index");
-		$table->dropIndex("timemanager_share_unique_index");
-		$table->addUniqueIndex(
-			["object_uuid", "entity_type", "author_user_id", "recipient_id", "recipient_type"],
-			"timemanager_share_unique_index"
-		);
 
 		return $schema;
 	}
@@ -71,8 +59,7 @@ class Version3009Date20231202210000 extends SimpleMigrationStep
 	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
 	 * @param array   $options
 	 */
-	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void
-	{
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
 		$query = $this->db->getQueryBuilder();
 		$query->update("timemanager_share")
 			->set("recipient_id", "recipient_user_id")
--- lib/Migration/Version3009Date20230918152300.php
+++ lib/Migration/Version3009Date20230918152300.php
@@ -7,30 +7,19 @@ namespace OCA\TimeManager\Migration;
 
 use Closure;
 use OCP\DB\ISchemaWrapper;
-use OCP\IDBConnection;
 use OCP\Migration\IOutput;
 use OCP\Migration\SimpleMigrationStep;
 
 /**
  * Auto-generated migration step: Please modify to your needs!
  */
-class Version3009Date20231202211500 extends SimpleMigrationStep
-{
-	/** @var IDBConnection */
-	private $db;
-
-	public function __construct(IDBConnection $db)
-	{
-		$this->db = $db;
-	}
-
+class Version3009Date20230918152300 extends SimpleMigrationStep {
 	/**
 	 * @param IOutput $output
 	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
 	 * @param array   $options
 	 */
-	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void
-	{
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
 	}
 
 	/**
@@ -39,12 +28,26 @@ class Version3009Date20231202211500 extends SimpleMigrationStep
 	 * @param array   $options
 	 * @return null|ISchemaWrapper
 	 */
-	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper
-	{
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
 		/** @var ISchemaWrapper $schema */
 		$schema = $schemaClosure();
 
 		$table = $schema->getTable("timemanager_share");
+
+		$table->changeColumn("recipient_id", [
+			"notnull" => true
+		]);
+		$table->changeColumn("recipient_type", [
+			"notnull" => true
+		]);
+
+		$table->dropIndex("timemanager_share_with_index");
+		$table->addIndex(["recipient_id", "recipient_type"], "timemanager_share_with_index");
+		$table->dropIndex("timemanager_share_unique_index");
+		$table->addUniqueIndex(
+			["object_uuid", "entity_type", "author_user_id", "recipient_id", "recipient_type"],
+			"timemanager_share_unique_index"
+		);
 
 		$table->dropColumn("recipient_user_id");
 
@@ -56,7 +59,6 @@ class Version3009Date20231202211500 extends SimpleMigrationStep
 	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
 	 * @param array   $options
 	 */
-	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void
-	{
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
 	}
 }
