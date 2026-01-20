996d563961e3981d6a0e30ec175cbbc857e36380
Thomas Ebert <thomas.ebert@te-online.net>
2026-01-20 22:12:35 +0100

    fix(PHP Svelte): Disable SSR of Svelte components, as it causes issues
    Incomplete HTML generated by the regular expressions to do basic SSR,
    led to failed "hydrations" of those components and incomplete content
    replacements when navigating with Pjax.

--- lib/Helper/PHP_Svelte.php
+++ lib/Helper/PHP_Svelte.php
@@ -3,47 +3,13 @@
 namespace OCA\TimeManager\Helper;
 
 class PHP_Svelte {
-	static function render_template(string $filename = "", array $props = []) {
-		$location = dirname(__FILE__) . "/../../js/views/" . $filename;
-		if ($location) {
-			$template = file_get_contents(dirname(__FILE__) . "/../../js/views/" . $filename);
-			if ($template) {
-				// Strip script and style tags
-				$rendered_template = preg_replace('@<(script|style)[^>]*?>.*?</\\1>@si', "", $template);
-				// Remove binds
-				$rendered_template = preg_replace("/bind\:(.*?)={(.*?)}/", "", $rendered_template);
-				// Remove if statements
-				$rendered_template = preg_replace("/{\#if((?s).*){\/if}/", "", $rendered_template);
-				// Replace all variables in tag attributes and wrap in quotes
-				$rendered_template = PHP_Svelte::replace_in_template("/={(.*?)}/", $props, $rendered_template, true);
-				// Replace all other variables
-				$rendered_template = PHP_Svelte::replace_in_template("/{(.*?)}/", $props, $rendered_template, false);
-
-				return $rendered_template;
-			}
-		}
-	}
-
-	/**
-	 * Replaces all `{props}` in a template string.
-	 */
-	static function replace_in_template(string $regex, array $props, string $template, bool $quotes): string {
-		preg_match_all($regex, $template, $matches);
-		if ($matches && count($matches) > 1) {
-			foreach ($matches[1] as $match) {
-				if (isset($props[$match])) {
-					$replacement = is_string($props[$match]) ? $props[$match] : json_encode($props[$match]);
-					if ($quotes) {
-						$template = preg_replace("/{" . $match . "}/", '"' . $replacement . '"', $template);
-					} else {
-						$template = preg_replace("/{" . $match . "}/", $replacement, $template);
-					}
-				} else {
-					$template = str_replace("{" . $match . "}", "", $template);
-				}
-			}
-		}
-
-		return $template;
+    /**
+     * @deprecated This function is a no-op.
+     * SSR of regular Svelte components using simple regular expressions did not work very well.
+     * Due to Svelte's every improving syntax, it led to incomplete HTML.
+     * This was in turn causing rendering issues. Therefore, this experiment ends here.
+     */
+	static function render_template(string $filename = "", array $props = []): string {
+		return "";
 	}
 }
