1c956d78f73e2bd3eddc232a4fb6e82b8c7e7cd5
Thomas Ebert <thomas.ebert@te-online.net>
2026-02-01 19:38:56 +0100

    chore(tests): Update cypress and configure Firefox to use English when testing

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -12,6 +12,13 @@ export default defineConfig({
 		// We've imported your old cypress plugins here.
 		// You may want to clean this up later by importing these.
 		setupNodeEvents(on, config) {
+			on("before:browser:launch", (browser, launchOptions) => {
+				if (browser.family === "firefox") {
+					launchOptions.preferences["intl.accept_languages"] = "en-en";
+				}
+				return launchOptions;
+			});
+
 			return require("./cypress/plugins/index.js")(on, config);
 		},
 	},
